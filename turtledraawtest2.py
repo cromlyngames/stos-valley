ab5b2f6f39c0760843d49589c4612dda98d0e59d
Patrick Barry <cromlyngames@gmail.com>
2016-04-11 18:36:10 +0100

    Update turtledraawtest2.py

--- turtledraawtest2.py
+++ turtledraawtest2.py
@@ -1,8 +1,20 @@
 import turtle
 import random
+from PIL import Image
 from tkinter import *
-   
-testingmon = [0,
+
+mirror=1       #used to reflect the turtle
+scale =100  #used to scale the size of the monster
+looper=1    #if 0 then program will complete
+population =1 #looper 
+
+  
+testingdetail = [0,
+                 0.5, 10, 0.9, #eyeratio, frownA, eyesizeratio,
+                 0.5,  0.3                   #nosesize, #mouthsize
+                 ]
+
+testingmon = [testingdetail,
     0.4,                         #crownD,
     90, 0.15, 45, 90, 45, 0.1,   #ear1A,ear2D,ear3A,ear4A,ear5A,ear5D,
     0.2, 60, 0.17,               #cheekD,jawlineA,shoulderD,
@@ -12,14 +24,18 @@ testingmon = [0,
     ]
 
 
-mirror=1       #used to reflect the turtle
-scale =100  #used to scale the size of the monster
-looper=1    #if 0 then program will complete
-population =1 #looper counting number of good monsters the program will make at a time
+
+
 
 
 def randommonMaker():
-    randommon = [0,
+
+    randomdetail=[0,
+        random.random(), random.randint(10, 70), random.random()*1.5,  #eyeratio, frownA, eyesizeratio
+        random.random(), random.random()*0.6  #nosesize, #mouthsize
+        ]
+
+    randommon = [randomdetail,
         random.random()*0.4,                         #crownD,
         random.randint(45, 135), random.random()*0.15, random.randint(15, 75), random.randint(45, 135), random.randint(15, 75), random.random()*0.1,   #ear1A,ear2D,ear3A,ear4A,ear5A,ear5D,
         random.random()*0.2, random.randint(10, 70), random.random()*0.17,               #cheekD,jawlineA,shoulderD,
@@ -36,7 +52,6 @@ def drawmonster(mirror, scale, generikmon):
   #  for i in range(25):
    #       print(generikmon[i], i)
     badnesscount=0
-
     turtle.right(mirror*90)                 #requires turtle to be facing upwards       
     turtle.forward(scale*generikmon[1])  #crownD
     turtle.left(mirror*generikmon[2])     #ear1A
@@ -112,23 +127,68 @@ def drawmonster(mirror, scale, generikmon):
     turtle.forward(scale*generikmon[21])
     turtle.left(mirror*(generikmon[20])-90)
     turtle.forward(tailwidth)
-    if turtle.xcor()*mirror<0:
-        badnesscount=badnesscount+5
+    drawmonsterdetails(mirror,scale,randommon) #ADD the details
+   # looper=drawmonsterdetails(mirror,scale,testingmon) #ADD the details
     return(badnesscount)
+    
+def drawmonsterdetails(mirror, scale, generikmon):
+    detailsbit = generikmon[0]
+      #for i in range(25):
+       # print(generikmon[i], i)
+    badnesscount=0
+
+    ### draw the eyes
+    turtle.penup()
+    turtle.home()
+   # print(turtle.xcor(),turtle.ycor())
+    turtle.setheading(90)
+    turtle.right(mirror*90)                 #requires turtle to be facing upwards       
+    turtle.forward(scale*generikmon[1]*detailsbit[1]) #crownD, #eyeratio
+    turtle.setheading(270)
+    turtle.forward(scale*generikmon[8]*(1-detailsbit[1])) #cheekD, #eyeratio
+    turtle.shape("circle")
+    turtle.resizemode("user")
+    turtle.shapesize(detailsbit[3],detailsbit[1],1)
+   # print(turtle.shapesize())
+    turtle.tilt(mirror*detailsbit[2])
+    turtle.stamp()
+    turtle.tilt(-1*mirror*detailsbit[2]) #takes the tilt off the turtle to avoid later confusion
+    turtle.shapesize(1,1,1)             #takes off the eye size, i think.
+    ### draw the nose
+    turtle.penup()
+    turtle.home()
+    turtle.setheading(270)
+    turtle.forward(scale*generikmon[8]) #cheekD
+    turtle.shape("triangle")
+    turtle.shapesize(detailsbit[4],detailsbit[4],1)
+    turtle.stamp()
+    #and from the nose, the mouth
+    turtle.pendown()
+    turtle.forward(scale*generikmon[10]*detailsbit[4]+detailsbit[4]) #shouldD below the nose
+    turtle.left(mirror*90)
+    turtle.forward(scale*generikmon[1]*detailsbit[5]) #crownD   #width of mouth
+    turtle.penup()
+    turtle.home()
+    return()
+
+
 
 ##########################################
-while (population <10):
-    print(population)
+while (population < 10):
+   # print(population)
     looper=1
     while (looper>0):    #will keep drawing monsters until a good drawing achieved
         mirror=1
+        turtle.penup()
         turtle.home()
         turtle.clear()
         turtle.setheading(90) #sets drawing canvas up
         turtle.hideturtle()
-        turtle.speed(0)
+        turtle.speed(10)
+        turtle.pendown()
         randommon =randommonMaker()
         looper=drawmonster(mirror, scale, randommon) #if badness occurs, looper=/= 0
+ #      looper=drawmonster(mirror, scale, testingmon)
         #print(looper)
         turtle.penup()
         turtle.home()
@@ -137,13 +197,20 @@ while (population <10):
         mirror=-1
         drawmonster(mirror, scale, randommon)  #draws second half of monster
 
-    print("made")
+   # print("made")
     ts = turtle.getscreen()
     filenamer = str(population)  #turns population iteration into a string for filenameig
-    filenamer = filenamer+".eps"
-    print("filename =")
-    ts.getcanvas().postscript(file=filenamer)
-    print (filenamer)
+    filenamerEPS = filenamer+".eps"
+    #print("filename =")
+    ps=ts.getcanvas().postscript(file=filenamerEPS)
+   # im=Image.open(io.BytesIO(ps.encode('utf-8')))
+  #  im=im.resize((2560.1600), Image.ANTIALIAS)
+   # quality_val=95
+  #  sharp = ImageEnhance.Sharpness(im)
+  #  sharp.enhance(2.0).save(filenamer+ ".png", 'PNG')
+ #   print (filenamer)
     population=population+1
     print("pop= ", population)
+
+    
         
