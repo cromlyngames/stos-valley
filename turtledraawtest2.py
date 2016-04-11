98202a6e42ddd3175853230a048fd7b06bd2b94a
Patrick Barry <cromlyngames@gmail.com>
2016-04-11 23:20:12 +0100

    Update turtledraawtest2.py
    testingmon & functions converted to dictionary.
    TODO: convert randommon to dictionary.

--- turtledraawtest2.py
+++ turtledraawtest2.py
@@ -1,6 +1,5 @@
 import turtle
 import random
-from PIL import Image
 from tkinter import *
 
 mirror=1       #used to reflect the turtle
@@ -8,20 +7,20 @@ scale =100  #used to scale the size of the monster
 looper=1    #if 0 then program will complete
 population =1 #looper 
 
+testingdetail = { 'expansionslot': 0,
+              'eyeratio': 0.5,   'frownA': 10,     'eyesize': 0.9,
+              'nosesize': 0.5,   'mouthsize': 0.3,  
+            }
   
-testingdetail = [0,
-                 0.5, 10, 0.9, #eyeratio, frownA, eyesizeratio,
-                 0.5,  0.3                   #nosesize, #mouthsize
-                 ]
+testingmon ={ 'detail0':testingdetail,
+              'crownD':0.4,
+              'ear1A':90,       'ear2D':0.15,   'ear3A':45,       'ear4A':90, 'ear5A':45, 'ear6D':0.1,   #ear1A,ear2D,ear3A,ear4A,ear5A,ear5D,
+              'cheekD':0.2,     'jawlineA':60,  'shoulderD':0.17,               #cheekD,jawlineA,shoulderD,
+              'shoulderA':60,   'thighD':0.3,   'elbowA':10,      'shinD':0.3,            #shoulderA, thighD, elbowA, shinD
+              'NumofToes':3,    'frontbackratio':0.75, 'toelengthD':0.12,               #NumOfToes, frontbackratio, toelengthD, 
+              'tailbodyratio':0.1, 'tailspineD':0.5, 'tailtipA':80, 'tailtipD': 0.1           #tailbodywidthratio, tailspineD, tailtipA, tailtipD
+              }
 
-testingmon = [testingdetail,
-    0.4,                         #crownD,
-    90, 0.15, 45, 90, 45, 0.1,   #ear1A,ear2D,ear3A,ear4A,ear5A,ear5D,
-    0.2, 60, 0.17,               #cheekD,jawlineA,shoulderD,
-    60, 0.3, 10, 0.3,            #shoulderA, thighD, elbowA, shinD
-    3, 0.75, 0.12,               #NumOfToes, frontbackratio, toelengthD, 
-    0.1, 0.5, 80, 0.1           #tailbodywidthratio, tailspineD, tailtipA, tailtipD
-    ]
 
 
 
@@ -53,18 +52,18 @@ def drawmonster(mirror, scale, generikmon):
    #       print(generikmon[i], i)
     badnesscount=0
     turtle.right(mirror*90)                 #requires turtle to be facing upwards       
-    turtle.forward(scale*generikmon[1])  #crownD
-    turtle.left(mirror*generikmon[2])     #ear1A
-    turtle.forward(scale*generikmon[3])  #ear2D
-    turtle.right(mirror*generikmon[4])    #ear3A
-    turtle.forward(scale*generikmon[3])  #ear2D
-    turtle.right(mirror*generikmon[5])    #ear4A
-    turtle.forward(scale*generikmon[7])  #ear5D
-    turtle.right(mirror*generikmon[6])    #ear5A
-    turtle.forward(scale*generikmon[7])  #ear5D
+    turtle.forward(scale*generikmon['crownD'])  #crownD
+    turtle.left(mirror*generikmon['ear1A'])     #ear1A
+    turtle.forward(scale*generikmon['ear2D'])  #ear2D
+    turtle.right(mirror*generikmon['ear3A'])    #ear3A
+    turtle.forward(scale*generikmon['ear2D'])  #ear2D
+    turtle.right(mirror*generikmon['ear4A'])    #ear4A
+    turtle.forward(scale*generikmon['ear6D'])  #ear6D
+    turtle.right(mirror*generikmon['ear5A'])    #ear5A
+    turtle.forward(scale*generikmon['ear6D'])  #ear5D
     #into the cheek
     turtle.setheading(270)         #fixed
-    turtle.forward(scale*generikmon[8])  #cheekD
+    turtle.forward(scale*generikmon['cheekD'])  #cheekD
     if turtle.xcor()*mirror<0:
         badnesscount=badnesscount+1
         return(badnesscount)
@@ -72,10 +71,10 @@ def drawmonster(mirror, scale, generikmon):
         badnesscount=badnesscount+2
         return(badnesscount)
     #along the jaw and then the shoulder
-    turtle.left(mirror*generikmon[9])     #jawlineA
-    turtle.forward(scale*generikmon[10]) #shoulderD
-    turtle.right(mirror*(180-generikmon[9]))
-    turtle.forward(scale*generikmon[10]*2)
+    turtle.left(mirror*generikmon['jawlineA'])     #jawlineA
+    turtle.forward(scale*generikmon['shoulderD']) #shoulderD
+    turtle.right(mirror*(180-generikmon['jawlineA']))
+    turtle.forward(scale*generikmon['shoulderD']*2)
     if turtle.xcor()*mirror<0:
         badnesscount=badnesscount+3
         return(badnesscount)
@@ -83,56 +82,56 @@ def drawmonster(mirror, scale, generikmon):
         badnesscount=badnesscount+2
         return(badnesscount)
     #down the arm
-    turtle.left(mirror*generikmon[11])    #shoulderA
-    turtle.forward(scale*generikmon[12]) #thighD
-    turtle.left(mirror*generikmon[13])    #elbowA
-    turtle.forward(scale*generikmon[14])#shinD
+    turtle.left(mirror*generikmon['shoulderA'])    #shoulderA
+    turtle.forward(scale*generikmon['thighD']) #thighD
+    turtle.left(mirror*generikmon['elbowA'])    #elbowA
+    turtle.forward(scale*generikmon['shinD'])#shinD
     # into the toes!
-    NumofToes = generikmon[15]
+    NumofToes = generikmon['NumofToes']
     fullwidth= turtle.xcor()
-    tailwidth= fullwidth*generikmon[18] #talibodywidthratio,
+    tailwidth= fullwidth*generikmon['tailbodyratio'] #talibodywidthratio,
     pawswidth= fullwidth-tailwidth
-    frontpawwidth=pawswidth*generikmon[16] #fronttobackratio
+    frontpawwidth=pawswidth*generikmon['frontbackratio'] #fronttobackratio
     toewidth=frontpawwidth/(NumofToes)
     if turtle.ycor()>0:
         badnesscount=badnesscount+2
         return(badnesscount)
     for i in range(NumofToes):
         turtle.setheading(270)
-        turtle.forward(scale*generikmon[17])#toelengthD
+        turtle.forward(scale*generikmon['toelengthD'])#toelengthD
         turtle.right(mirror*90)
         turtle.forward(mirror*toewidth)  #mirror required here since xcord is negative on second hlaf
         turtle.right(mirror*90)
-        turtle.forward(scale*generikmon[17]) #toelegnth - going back up               
+        turtle.forward(scale*generikmon['toelengthD']) #toelegnth - going back up               
       #into the rear toes. # all lengths scaled by generikmon[16] = frontotbackratio
     toewidth=(pawswidth-frontpawwidth)/NumofToes  #redefines toe width for backpaws.
     for i in range(NumofToes):
         turtle.setheading(270)
-        turtle.forward(scale*generikmon[16]*generikmon[17])#toelengthD
+        turtle.forward(scale*generikmon['frontbackratio']*generikmon['toelengthD'])#toelengthD
         turtle.right(mirror*90)
         turtle.forward(mirror*toewidth)
         turtle.right(mirror*90)
-        turtle.forward(scale*generikmon[16]*generikmon[17]) #toelegnth - going back up   
+        turtle.forward(scale*generikmon['frontbackratio']*generikmon['toelengthD']) #toelegnth - going back up   
     if turtle.xcor()*mirror<0:
         badnesscount=badnesscount+4
         return(badnesscount)
     # and the tail
     turtle.setheading(270)
-    turtle.forward(scale*generikmon[19]) #talespine
-    turtle.left(mirror*generikmon[20])        #tailtipA
-    turtle.forward(scale*generikmon[21])   #tailtipD
-    turtle.right(mirror*generikmon[20])
-    turtle.forward(scale*generikmon[21])
-    turtle.right(mirror*generikmon[20])
-    turtle.forward(scale*generikmon[21])
-    turtle.left(mirror*(generikmon[20])-90)
+    turtle.forward(scale*generikmon['tailspineD']) #talespine
+    turtle.left(mirror*generikmon['tailtipA'])        #tailtipA
+    turtle.forward(scale*generikmon['tailtipD'])   #tailtipD
+    turtle.right(mirror*generikmon['tailtipA'])
+    turtle.forward(scale*generikmon['tailtipD'])
+    turtle.right(mirror*generikmon['tailtipA'])
+    turtle.forward(scale*generikmon['tailtipD'])
+    turtle.left(mirror*(generikmon['tailtipA'])-90)
     turtle.forward(tailwidth)
-    drawmonsterdetails(mirror,scale,randommon) #ADD the details
-   # looper=drawmonsterdetails(mirror,scale,testingmon) #ADD the details
+   #drawmonsterdetails(mirror,scale,randommon) #ADD the details
+    looper=drawmonsterdetails(mirror,scale,testingmon) #ADD the details
     return(badnesscount)
     
 def drawmonsterdetails(mirror, scale, generikmon):
-    detailsbit = generikmon[0]
+    detailsbit = generikmon['detail0']
       #for i in range(25):
        # print(generikmon[i], i)
     badnesscount=0
@@ -143,30 +142,30 @@ def drawmonsterdetails(mirror, scale, generikmon):
    # print(turtle.xcor(),turtle.ycor())
     turtle.setheading(90)
     turtle.right(mirror*90)                 #requires turtle to be facing upwards       
-    turtle.forward(scale*generikmon[1]*detailsbit[1]) #crownD, #eyeratio
+    turtle.forward(scale*generikmon['crownD']*detailsbit['eyeratio']) #crownD, #eyeratio
     turtle.setheading(270)
-    turtle.forward(scale*generikmon[8]*(1-detailsbit[1])) #cheekD, #eyeratio
+    turtle.forward(scale*generikmon['cheekD']*(1-detailsbit['eyeratio'])) #cheekD, #eyeratio
     turtle.shape("circle")
     turtle.resizemode("user")
-    turtle.shapesize(detailsbit[3],detailsbit[1],1)
+    turtle.shapesize(detailsbit['eyesize'],detailsbit['eyesize']*detailsbit['eyeratio']*4,1)
    # print(turtle.shapesize())
-    turtle.tilt(mirror*detailsbit[2])
+    turtle.tilt(mirror*detailsbit['frownA'])
     turtle.stamp()
-    turtle.tilt(-1*mirror*detailsbit[2]) #takes the tilt off the turtle to avoid later confusion
+    turtle.tilt(-1*mirror*detailsbit['frownA']) #takes the tilt off the turtle to avoid later confusion
     turtle.shapesize(1,1,1)             #takes off the eye size, i think.
     ### draw the nose
     turtle.penup()
     turtle.home()
     turtle.setheading(270)
-    turtle.forward(scale*generikmon[8]) #cheekD
+    turtle.forward(scale*generikmon['cheekD']) #cheekD
     turtle.shape("triangle")
-    turtle.shapesize(detailsbit[4],detailsbit[4],1)
+    turtle.shapesize(detailsbit['nosesize'],detailsbit['nosesize'],1)
     turtle.stamp()
     #and from the nose, the mouth
     turtle.pendown()
-    turtle.forward(scale*generikmon[10]*detailsbit[4]+detailsbit[4]) #shouldD below the nose
+    turtle.forward(scale*generikmon['shoulderD']*detailsbit['nosesize']+detailsbit['nosesize']) #shouldD below the nose
     turtle.left(mirror*90)
-    turtle.forward(scale*generikmon[1]*detailsbit[5]) #crownD   #width of mouth
+    turtle.forward(scale*generikmon['crownD']*detailsbit['mouthsize']) #crownD   #width of mouth
     turtle.penup()
     turtle.home()
     return()
@@ -187,15 +186,15 @@ while (population < 10):
         turtle.speed(10)
         turtle.pendown()
         randommon =randommonMaker()
-        looper=drawmonster(mirror, scale, randommon) #if badness occurs, looper=/= 0
- #      looper=drawmonster(mirror, scale, testingmon)
+#        looper=drawmonster(mirror, scale, randommon) #if badness occurs, looper=/= 0
+        looper=drawmonster(mirror, scale, testingmon)
         #print(looper)
         turtle.penup()
         turtle.home()
         turtle.pendown()
         turtle.setheading(90)
         mirror=-1
-        drawmonster(mirror, scale, randommon)  #draws second half of monster
+        #drawmonster(mirror, scale, randommon)  #draws second half of monster
 
    # print("made")
     ts = turtle.getscreen()
